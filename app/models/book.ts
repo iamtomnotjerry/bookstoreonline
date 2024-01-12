a24515fdb1e6f2ec747a6792c008138059e9fb19
honguyenminh <honguyenminh@outlook.com>
2024-01-13 00:32:44 +0700

    Update book model

--- app/models/book.ts
+++ app/models/book.ts
@@ -1,38 +1,92 @@
 import mongoose, { Schema, Document } from 'mongoose';
 
-interface Book extends Document {
+enum CoverType {
+  Soft = "soft",
+  Hard = "hard",
+}
+
+interface IBook extends Document {
+  bookId: mongoose.Types.ObjectId;
   title: string;
   author: string;
   price: number;
-  type: string;
-  imageUrl: string;
-  userEmail: string;
-  discription: string;
-  pdfId: mongoose.Types.ObjectId;
+  description: string;
+  provider: string;
+  publisher: string;
+  publishYear: number;
+  language: string;
+  weight: number;
+  dimensionsInCm: { x: number, y: number, z:number }
+  pageCount: number;
+  coverType: CoverType;
+  coverImageUrl: string;
+  tags: string[];
 }
 
-const bookSchema = new Schema<Book>(
-  {
-    title: {
-      type: String,
-      required: true,
-    },
-    author: {
-      type: String,
-      required: true,
-    },
-    imageUrl: {
-      type: String,
-      required: true,
-    },
-    pdfId: {
-      type: Schema.Types.ObjectId, // Explicitly specify the type
-      required: true,
-    },
+const BookSchema = new Schema<IBook>({
+  bookId: {
+    type: Schema.Types.ObjectId,
+    required: true,
   },
-  { timestamps: true }
-);
+  title: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  provider: {
+    type: String,
+    required: true,
+  },
+  publisher: {
+    type: String,
+    required: true,
+  },
+  publishYear: {
+    type: Number,
+    required: true,
+  },
+  language: {
+    type: String,
+    required: true,
+  },
+  weight: {
+    type: Number,
+    required: true,
+  },
+  dimensionsInCm: {
+    type: { x: Number, y: Number, z: Number },
+    required: true,
+  },
+  pageCount: {
+    type: Number,
+    required: true,
+  },
+  coverType: {
+    type: String,
+    required: true,
+    enum: [ "soft", "hard" ],
+  },
+  coverImageUrl: {
+    type: String,
+    required: true,
+  },
+  tags: {
+    type: [String],
+    required: true,
+  },
+});
 
-const BookModel = mongoose.models.Book as mongoose.Model<Book> || mongoose.model<Book>('Book', bookSchema);
+const BookModel = mongoose.models.Book as mongoose.Model<IBook> || mongoose.model<IBook>('Book', BookSchema);
 
-export default BookModel;
+export default { BookModel };
