6f8456bc70c45262846bed21833f5e5f32bc1a82
I am Tom <120075730+iamtomnotjerry@users.noreply.github.com>
2024-01-12 09:11:32 +0700

    basic struture website
    homepage, cartpage, userpage, adminpage, bookdetailpage

--- app/components/NavLinks.tsx
+++ app/components/NavLinks.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { useContext } from "react";
+import React, { useContext } from "react";
 import { StoreContext } from "@/app/context";
 import Link from "next/link";
 import { usePathname } from 'next/navigation';
@@ -8,14 +8,14 @@ import { useSession } from 'next-auth/react';
 import { 
   CubeTransparentIcon,
   BookOpenIcon, 
-  CogIcon, 
+  CogIcon, UserIcon, ShoppingCartIcon
 } from "@heroicons/react/24/outline";
 
 const links = [
-  
-  { name: 'Books', href: '/store', icon: BookOpenIcon },
-  { name: 'Settings', href: '/store/settings', icon: CogIcon },
-  { name: 'Admin', href: '/store/admin', icon: CubeTransparentIcon },
+  { name: 'Books', href: '/', icon: BookOpenIcon },
+  { name: 'Cart', href: '/cart', icon: ShoppingCartIcon },
+  { name: 'User', href: '/user', icon: UserIcon },
+  { name: 'Admin', href: '/admin', icon: CubeTransparentIcon },
 ];
 
 const NavLinks = () => {
@@ -32,19 +32,22 @@ const NavLinks = () => {
       {links.map((link) => {
         const IconComponent = link.icon;
         const isActive = pathname === link.href;
-  
+
         // Check if the link is "Admin" and the user is not an admin, then skip rendering
         if (link.name === 'Admin' && !isAdmin) {
           return null;
         }
-  
+
+        // Conditionally render the "User" link only if the user is signed in
+        if (link.name === 'User' && !userEmail) {
+          return null;
+        }
+
         return (
-          
           <Link
             prefetch={false}
             key={link.name}
             href={link.href}
-            
             className={`flex h-[48px] grow items-center justify-center gap-2 rounded-md p-3 text-sm font-medium 
               hover:bg-sky-100 hover:text-pink-400 md:flex-none md:justify-start md:p-2 md:px-3 
               ${isActive ? 'bg-sky-100 text-pink-400' : 'bg-gray-50 text-black'}`}
@@ -61,8 +64,5 @@ const NavLinks = () => {
     </>
   );
 };
-
-
-
 
 export default NavLinks;
