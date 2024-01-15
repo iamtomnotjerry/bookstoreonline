14e3f6ff3e78e81a96a8a84e6d16448cdeacc2a1
honguyenminh <honguyenminh@outlook.com>
2024-01-15 12:38:57 +0700

    Re-implement authorization

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,57 @@
+import NextAuth from 'next-auth/next';
+import CredentialProvider from 'next-auth/providers/credentials';
+import { connectMongoDB } from '@/app/lib/mongodb-connection-module';
+import bcrypt from 'bcryptjs';
+import User from '@/app/models/user';
+import { NextAuthOptions } from 'next-auth';
+
+export const authOptions : NextAuthOptions = {
+  providers: [
+    CredentialProvider({
+      name: 'credentials',
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" }
+      },
+      authorize: async (credentials, _req) => {
+        const { email, password } = credentials!;
+        try {
+          await connectMongoDB();
+          const user = await User.findOne({ email });
+
+          if (!user) {
+            return null;
+          }
+
+          const passwordsMatch = await bcrypt.compare(
+            password,
+            user.password
+          );
+
+          if (!passwordsMatch) {
+            return null;
+          }
+
+          return {
+            email: user.email,
+            id: user._id,
+          };
+        } catch (error) {
+          console.error("Error:", error);
+          throw error; // Rethrow the error for more information
+        }
+      },
+    }),
+  ],
+  session: {
+    strategy: 'jwt',
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  pages: {
+    signIn: '/signin',
+  },
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
--- app/api/books/[id]/route.ts
+++ app/api/books/[id]/route.ts
@@ -1,57 +1,100 @@
 import Book from "@/app/models/book";
+import User from "@/app/models/user";
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
-export async function GET(
-  req: NextRequest,
-  { params }: { params: { id: string }})
-{
-  const id = params.id;
-  try {
-    const book = await Book.findById(id);
-    if (!book) {
-      return NextResponse.json({ error: 'Book not found' }, { status: 404 });
-    }
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
-    return NextResponse.json(book , { status: 200 });
-  } catch (error) {
-    console.error('Error fetching book:', error);
-    return NextResponse.json({ error: error }, { status: 500 });
-  }
+async function authorizeAdmin() : Promise<NextResponse | null> {
+    const session = await getServerSession(authOptions);
+    if (session?.user?.email == null) {
+        return NextResponse.json(
+            { error: "Unauthorized operation, user not logged in" },
+            { status: 401 }
+        );
+    }
+    const user = await User.findOne({ email: session?.user?.email });
+    if (user == null) {
+        const message =
+            "Logged in user not found in database. This shouldn't happen";
+        console.error(message);
+        throw message;
+    }
+    if (!user.isAdmin) {
+        return NextResponse.json(
+            { error: "Unauthorized operation, forbidden" },
+            { status: 401 }
+        );
+    }
+    return null;
 }
-export async function DELETE(
-  req: NextRequest,
-  { params }: { params: { id: string }})
-{
-  // !TODO: Authorize user here. Only admins
-  const id = params.id;
-  try {
-    const book = await Book.findByIdAndDelete(id);
-    if (!book) {
-      return NextResponse.json({ error: 'Book not found' }, { status: 404 });
-    }
 
-    return NextResponse.json({ message: "Deleted", book } , { status: 200 });
-  } catch (error) {
-    console.error('Error fetching book:', error);
-    return NextResponse.json({ error: error }, { status: 500 });
-  }
+export async function GET(
+    req: NextRequest,
+    { params }: { params: { id: string } }
+) {
+    const id = params.id;
+    try {
+        const book = await Book.findById(id);
+        if (!book) {
+            return NextResponse.json(
+                { error: "Book not found" },
+                { status: 404 }
+            );
+        }
+
+        return NextResponse.json(book, { status: 200 });
+    } catch (error) {
+        console.error("Error fetching book:", error);
+        return NextResponse.json({ error: error }, { status: 500 });
+    }
+}
+
+export async function DELETE(
+    req: NextRequest,
+    { params }: { params: { id: string } }
+) {
+    const result = authorizeAdmin();
+    if (result != null)
+        return result;
+
+    const id = params.id;
+    try {
+        const book = await Book.findByIdAndDelete(id);
+        if (!book) {
+            return NextResponse.json(
+                { error: "Book not found" },
+                { status: 404 }
+            );
+        }
+
+        return NextResponse.json({ message: "Deleted", book }, { status: 200 });
+    } catch (error) {
+        console.error("Error fetching book:", error);
+        return NextResponse.json({ error: error }, { status: 500 });
+    }
 }
 export async function PATCH(
-  req: NextRequest,
-  { params }: { params: { id: string }})
-{
-  // !TODO: Authorize user here. Only admins
-  const id = params.id;
-  try {
-    const body = req.json();
-    const book = await Book.findByIdAndUpdate(id, body, { new: true });
-    if (!book) {
-      return NextResponse.json({ error: 'Book not found' }, { status: 404 });
-    }
+    req: NextRequest,
+    { params }: { params: { id: string } }
+) {
+    const result = authorizeAdmin();
+    if (result != null)
+        return result;
 
-    return NextResponse.json({ message: "Updated", book } , { status: 200 });
-  } catch (error) {
-    console.error('Error fetching book:', error);
-    return NextResponse.json({ error: error }, { status: 500 });
-  }
+    const id = params.id;
+    try {
+        const body = req.json();
+        const book = await Book.findByIdAndUpdate(id, body, { new: true });
+        if (!book) {
+            return NextResponse.json(
+                { error: "Book not found" },
+                { status: 404 }
+            );
+        }
+
+        return NextResponse.json({ message: "Updated", book }, { status: 200 });
+    } catch (error) {
+        console.error("Error fetching book:", error);
+        return NextResponse.json({ error: error }, { status: 500 });
+    }
 }
